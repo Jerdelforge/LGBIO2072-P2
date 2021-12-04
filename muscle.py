54363e3f10cfd666e9383bafe33d9678ad7fc00d
petitro <48588783+petitro@users.noreply.github.com>
2021-12-04 17:36:21 +0100

    bousculé par un connard
    update

--- muscle.py
+++ muscle.py
@@ -11,14 +11,43 @@ Created on Tue Nov 30 10:59:53 2021
 import numpy as np
 import loadDataP2 as L
 import matplotlib.pyplot as plt
+import FiringRate
 
 dict= L.loadMuscle()
 
-pecto=dict["Deltoid"][0]
-forceX=dict["HandXForce"][50]
-extract=dict["extracted"]
-descr=dict["descriptions"]
-#forcetot=np.sqrt(forceX**2+forceY**2)
-print(extract[:,3])
-plt.plot(pecto)
-#plt.plot(forceX)
+vitesse=np.sqrt(dict["HandXVel"]**2+dict["HandYVel"]**2)
+force=np.sqrt(dict["HandXForce"]**2+dict["HandYForce"]**2)
+forceX=dict["HandXForce"]
+forceY=dict["HandYForce"]
+posX=dict["HandX"]
+posY=dict["HandY"]
+pos=np.sqrt(dict["HandX"]**2+dict["HandY"]**2)
+
+#plt.plot(forceY[359],label="Y")
+#plt.plot(forceX[359],label="X")
+#plt.show()
+#plt.legend()
+#plt.plot(posY[59],posX[59])
+print(len(pos))
+pecto=dict["Pectoralis"]
+delto=dict["Deltoid"]
+
+def mean(tab):
+    ret=np.zeros(len(tab[0]))
+    for i in range(0,len(tab)):
+        ret=np.add(ret,tab[i])
+    ret=ret/len(tab)
+    return ret
+
+pectomean1=mean(pecto[0:50])
+pectomean2=mean(pecto[50:55])
+pectomean3=mean(pecto[55:60])
+plt.plot(pectomean2,label="pas un connard qui me frappe le bras")
+#plt.plot(pectomean2,label="Un connard me frappe le bras +X")
+#plt.plot(pectomean3,label="Un connard me frappe le bras -X")
+plt.legend()
+plt.ylabel("Activité musculaire en Volt[mV]")
+plt.plot(mean(vitesse[50:55]))
+
+print(dict["descriptions"])
+

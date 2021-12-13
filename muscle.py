75afa46bf3a8b7bc12a63d5c84aa527bd2e6ce89
petitro <48588783+petitro@users.noreply.github.com>
2021-12-13 16:25:51 +0100

    First part of graphs
    Graphique sur EMG and kinematics

--- muscle.py
+++ muscle.py
@@ -12,6 +12,7 @@ import numpy as np
 import loadDataP2 as L
 import matplotlib.pyplot as plt
 import FiringRate
+import math
 
 dict= L.loadMuscle()
 
@@ -22,13 +23,6 @@ forceY=dict["HandYForce"]
 posX=dict["HandX"]
 posY=dict["HandY"]
 pos=np.sqrt(dict["HandX"]**2+dict["HandY"]**2)
-
-#plt.plot(forceY[359],label="Y")
-#plt.plot(forceX[359],label="X")
-#plt.show()
-#plt.legend()
-#plt.plot(posY[59],posX[59])
-print(len(pos))
 pecto=dict["Pectoralis"]
 delto=dict["Deltoid"]
 
@@ -39,21 +33,93 @@ def mean(tab):
     ret=ret/len(tab)
     return ret
 
-pectomean1=mean(pecto[0:50])
-pectomean2=mean(pecto[50:55])
-pectomean3=mean(pecto[55:60])
-plt.plot(pectomean2,label="pas un connard qui me frappe le bras")
-#plt.plot(pectomean2,label="Un connard me frappe le bras +X")
-#plt.plot(pectomean3,label="Un connard me frappe le bras -X")
-plt.legend()
-plt.ylabel("Activité musculaire en Volt[mV]")
-#vitesse=dict["HandXVel"]
-plt.plot(mean(vitesse[50:55]))
-plt.show()
-plt.plot(mean(forceX[50:55]))
-plt.plot(mean(forceY[50:55]))
-plt.show()
-plt.plot(mean(pos[50:55]))
+def meanT(dico,tab,type):
+    x=np.where(dico["extracted"][:,2]==type)  
+    tab=tab[x]
+    return mean(tab)
 
-print(dict["descriptions"])
+
+#Données selon différents Type
+pecto1=meanT(dict,pecto,1)
+delto1=meanT(dict,delto,1)
+ForceX1=meanT(dict,forceX,1)
+ForceY1=meanT(dict,forceY,1)
+vitesse1=meanT(dict,vitesse,1)
+PosX1=meanT(dict,posX,1)
+PosY1=meanT(dict,posY,1)
+
+pecto2=meanT(dict,pecto,2)
+delto2=meanT(dict,delto,2)
+ForceX2=meanT(dict,forceX,2)
+ForceY2=meanT(dict,forceY,2)
+vitesse2=meanT(dict,vitesse,2)
+PosX2=meanT(dict,posX,2)
+PosY2=meanT(dict,posY,2)
+
+
+pecto3=meanT(dict,pecto,3)
+delto3=meanT(dict,delto,3)
+ForceX3=meanT(dict,forceX,3)
+ForceY3=meanT(dict,forceY,3)
+vitesse3=meanT(dict,vitesse,3)
+PosX3=meanT(dict,posX,3)
+PosY3=meanT(dict,posY,3)
+
+
+
+#Graphiq"ues
+#Force an"d EMG Type1
+"""fig,ax1= plt.subplots()
+ax2 = ax1.twinx()
+ax1.plot(pecto1,label="Pectoral",color='r')
+ax1.plot(delto1,label="Deltoid",color='c')
+#vitesse=dict["HandXVel"]
+ax2.plot(ForceX1,color='g',label="F_X")
+ax2.plot(ForceY1,color='y',label="F_Y")
+#ax2.plot(deltomean1,label="muscle delto")
+ax1.legend()
+ax2.legend(loc=(0.825,0.65))
+ax1.set_ylabel("EMG activity[mV]")
+ax2.set_ylabel("Force [N]")
+ax1.set_xlabel("Time[ticks]")
+plt.show()"""
+
+#Vitesse and EMG
+
+fig,ax1= plt.subplots()
+ax2 = ax1.twinx()
+ax1.plot(pecto1,label="Pectoral",color='r')
+ax1.plot(delto1,label="Deltoid",color='c')
+ax2.plot(vitesse1,color='g',label="Velocity")
+#ax2.plot(deltomean1,label="muscle delto")
+ax1.legend()
+ax2.legend(loc=(0.757,0.725))
+ax1.set_ylabel("EMG activity[mV]")
+ax2.set_ylabel("Velocity[m/s]")
+ax1.set_xlabel("Time[ticks]")
+plt.show()
+
+
+#Vitesse and Force
+"""fig,ax1= plt.subplots()
+ax2 = ax1.twinx()
+ax1.plot(vitesse1,label="vitesse",color='g')
+
+#vitesse=dict["HandXVel"]
+ax2.plot(ForceX1,color='r',label="F_X")
+ax2.plot(ForceY1,color='y',label="F_Y")
+#ax2.plot(deltomean1,label="muscle delto")
+ax1.legend()
+ax2.legend(loc=(0.825,0.65))
+ax1.set_ylabel("Velocity[mV]")
+ax2.set_ylabel("Force [N]")
+ax1.set_xlabel("Time[ticks]")
+plt.show()"""
+
+
+
+"""plt.plot(mean(posY[55:60]),mean(posX[50:55]))
+plt.xlim(0.0,0.3)
+plt.ylim(0.0,0.3)
+plt.show()"""
 
